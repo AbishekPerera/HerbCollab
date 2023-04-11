c9c5c83735d6560b742fb042829a0577b0f21dab
Janani Mayadunna <87350830+IT21026898@users.noreply.github.com>
2023-04-11 09:19:54 +0530

    Routes added, header footer removed from routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Sample from "./pages/Sample";
 import Test from "./pages/Test";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
+import Dashboard from "./pages/System/Admin/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
       {/* ::::::::::::::::::::::::::::::::::::::::Customer Routes::::::::::::::::::::::::::::::::::::::::::::::::::: */}
       <Routes>
         <Route path="/" element={<Sample />} />
@@ -18,7 +15,7 @@ function App() {
 
         {/* ::::::::::::::::::::::::::::::::::::::::Admin Routes::::::::::::::::::::::::::::::::::::::::::::::::::: */}
         <Route path="/system/auth" element={<Test />} />
-        <Route path="/system/admin-dashboard" element={<Test />} />
+        <Route path="/system/admin-dashboard" element={<Dashboard />} />
         {/* ::::::::::::::::::::::::::::::::::::::::Admin Routes::::::::::::::::::::::::::::::::::::::::::::::::::: */}
 
         {/* ::::::::::::::::::::::::::::::::::::::::Seller Routes::::::::::::::::::::::::::::::::::::::::::::::::::: */}
@@ -26,8 +23,6 @@ function App() {
         <Route path="/system/seller-dashboard" element={<Test />} />
         {/* ::::::::::::::::::::::::::::::::::::::::Seller Routes::::::::::::::::::::::::::::::::::::::::::::::::::: */}
       </Routes>
-
-      <Footer />
     </div>
   );
 }
