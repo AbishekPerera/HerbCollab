2b9070faa4738123c4a00d8ec39b807789119ca7
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-04-15 23:07:17 +0530

    Merge pull request #4 from AbishekPerera/Janani-Mayadunna
    Janani mayadunna

--- frontend/src/pages/Sample.jsx
+++ frontend/src/pages/Sample.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Logo from "../img/Logo/bg.jpg";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const Sample = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      Sample
+      <Footer />
+    </div>
+  );
 };
 
 export default Sample;
--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 const Test = () => {
-  return <div>Testing this</div>;
+  return (
+    <div>
+      <Header />
+      Testing this
+      <Footer />
+    </div>
+  );
 };
 
 export default Test;
