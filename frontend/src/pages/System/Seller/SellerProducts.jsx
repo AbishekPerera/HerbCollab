696d3158be4fc851769aab82fd1be690a01e91a0
Kavindya Udunuwara <udunuwaraukc@gmail.com>
2023-04-18 21:46:59 +0530

    UpdateSellerProductPage

--- frontend/src/pages/System/Seller/SellerProducts.jsx
+++ frontend/src/pages/System/Seller/SellerProducts.jsx
@@ -50,7 +50,7 @@ const SellerProducts = () => {
 <div class="container bg-white">
     
     <div class="row">
-    <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
+        <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
             <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/12/Neroli-Jasmine-Hudrate-Treat-Rich-Body-Butter-1-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
                     <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen" ></span></li>
@@ -66,15 +66,15 @@ const SellerProducts = () => {
             <div class="price">Rs 6000.0</div>
         </div>
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
-        <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/07/Sal-saffron-vitamin-E-rich-Delicate-creme-facial-exfoliator-5-300x300.jpg" alt=""/>
+            <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/12/Neroli-Jasmine-Hudrate-Treat-Rich-Body-Butter-1-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen" ></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
-            <div class="tag bg-red">Body Creams</div>
+            <div class="tag bg-red">Facial Creams</div>
             <span class="tag1 bg-green"> 15</span>
-            <div class="title pt-4 pb-1">Neroli Jasmin</div>
+            <div class="title pt-4 pb-1">White Rice Cream</div>
             <div class="d-flex align-content-center justify-content-center">
               <p>Helo werewsrfe reffefef dfdgfdgfdg fgfdgfgf fgdgdgdf fggdfgfdgd</p>
             </div>
@@ -83,7 +83,7 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
             <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/12/Neroli-Jasmine-Hudrate-Treat-Rich-Body-Butter-1-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
@@ -98,7 +98,7 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
         <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/07/Sal-saffron-vitamin-E-rich-Delicate-creme-facial-exfoliator-5-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
@@ -113,7 +113,7 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
             <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/12/Neroli-Jasmine-Hudrate-Treat-Rich-Body-Butter-1-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
@@ -128,8 +128,8 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
         <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/07/Sal-saffron-vitamin-E-rich-Delicate-creme-facial-exfoliator-5-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
-                    <li class="icon mx-3"><span class="bi bi-trash"></span></li>
+                  <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
+                  <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
             <div class="tag bg-red">Body Creams</div>
@@ -143,7 +143,7 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
             <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/12/Neroli-Jasmine-Hudrate-Treat-Rich-Body-Butter-1-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
@@ -158,7 +158,7 @@ const SellerProducts = () => {
         <div class="col-lg-3 col-sm-6 d-flex flex-column align-items-center justify-content-center product-item my-3">
         <div class="product"> <img src="https://lk.spaceylon.com/wp-content/uploads/2022/07/Sal-saffron-vitamin-E-rich-Delicate-creme-facial-exfoliator-5-300x300.jpg" alt=""/>
                 <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
-                    <li class="icon "id="edit"><span class="bi bi-pen"></span></li>
+                    <li class="icon "id="edit"  onClick={handleUpdateProductModalShow} ><span class="bi bi-pen"></span></li>
                     <li class="icon mx-3"><span class="bi bi-trash"></span></li>
                 </ul>
             </div>
@@ -168,7 +168,7 @@ const SellerProducts = () => {
             <div class="d-flex align-content-center justify-content-center">
               <p>Helo werewsrfe reffefef dfdgfdgfdg fgfdgfgf fgdgdgdf fggdfgfdgd</p>
             </div>
-            <div class="price">Rs 6000.0</div>
+            <div class="price">Rs 6000.00</div>
         </div>
         
     </div>
