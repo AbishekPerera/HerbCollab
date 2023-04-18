86fe1ef44d4439db4434629d4e0f03badf19afaa
Kavindya Udunuwara <udunuwaraukc@gmail.com>
2023-04-18 21:37:35 +0530

    ADD PRoducts

---
+++ frontend/src/components/System/SellerProducts/AddProductModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Modal from 'react-bootstrap/Modal';
+
+const AddProductModal = ({show, handleClose}) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Add Product</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <div className="form-group">
+          <label htmlFor="ProductName">Product Name</label>
+          <input type="text" className="form-control"  placeholder="Enter product Name"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="ProductCategory">Product Category</label>
+          <input type="text" class="form-control"  placeholder="Enter product Category"/>
+        </div>
+        <div class="form-group">
+          <label for="ProductDescription">Product Description</label>
+          <input type="text" class="form-control"  placeholder="Enter product Description"/>
+        </div>
+        <div class="form-group">
+          <label for="ProductQuantity">Quantity</label>
+          <input type="number" class="form-control"  placeholder="Enter Quantity"/>
+        </div>
+        <div class="form-group">
+          <label for="ProductPrice">Price</label>
+          <input type="number" class="form-control"  placeholder="Price"/>
+        </div>
+        <div class="form-group">
+          <label for="ProductImage">Image</label>
+          <input type="text" class="form-control"  placeholder="Image URL"/>
+        </div>
+   
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="submit" class="btn btn-success" variant="secondary" Click={handleClose}>Add Product</button>
+  
+          <button class="btn btn-danger"  variant="primary" onClick={handleClose}>
+            Close
+          </button>
+          
+        </Modal.Footer>
+      </Modal>
+  );
+}
+export default AddProductModal;
--- frontend/src/pages/SystemLoginPage/system-Login.jsx
+++ frontend/src/pages/SystemLoginPage/system-Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./system-Login.css";
+import Header from "../../components/Header/Header";
 
 const SystemLogin = () => {
   const [isSignUpMode, setIsSignUpMode] = useState(false);
@@ -21,84 +22,69 @@ const SystemLogin = () => {
   };
 
   return (
-    <div className={`container-login ${isSignUpMode ? 'sign-up-mode' : ''}`}>
+
+    <><Header /><div className={`container-login ${isSignUpMode ? 'sign-up-mode' : ''}`}>
       <div className="forms-container">
         <div className="signin-signup">
           <form action="#" className="sign-in-form">
             <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="name" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
+            <input type="name" className="input-field" placeholder="UserName" />
+            <input type="password" className="input-field" placeholder="Password" />
+
             <input type="submit" value="Login" id="btnLogin" className="btnLogin solid" />
             <a href="" className="social-text">Fogot Password ? </a>
           </form>
           <form action="#" className="sign-up-form">
             <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="name" placeholder="User Name" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="name" placeholder="Store Name" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="number" placeholder="Mobile Number" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="textfileld" rows="2" colomns="2" placeholder="Address" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
+
+            <input type="name" className="input-field" placeholder="User Name" />
+
+            <input type="name" className="input-field" placeholder="Store Name" />
+
+            <input type="email" className="input-field" placeholder="Email" />
+
+            <input type="number" className="input-field" placeholder="Mobile Number" />
+
+            <input type="address" className="input-field" placeholder="Address" />
+
+            <input type="password" className="input-field" placeholder="Password" />
+
             <input type="submit" id="btnLogin" value="Sign up" />
-            
-            
+
+
           </form>
         </div>
       </div>
-     
+
       <div className="panels-container">
         <div className="panel left-panel" onClick={handleSignupMode}>
-      <div className="content">
-        <h3>New here ?</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-          ex ratione. Aliquid!
-        </p>
-        <button className="btnLogin transparent" id="sign-up-btnLogin" onClick={handleSigninClick}>
-          Sign up
-        </button>
-      </div>
+          <div className="content">
+            <h3>New here ?</h3>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!
+            </p>
+            <button className="btnLogin transparent" id="sign-up-btnLogin" onClick={handleSigninClick}>
+              Sign up
+            </button>
+          </div>
 
-    </div>
-    <div className="panel right-panel" onClick={handleSigninMode}>
-      <div className="content">
-        <h3>One of us ?</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          laboriosam ad deleniti.
-        </p>
-        <button className="btnLogin transparent" id="sign-in-btnLogin" onClick={handleSignupClick}>
-          Sign in
-        </button>
+        </div>
+        <div className="panel right-panel" onClick={handleSigninMode}>
+          <div className="content">
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button className="btnLogin transparent" id="sign-in-btnLogin" onClick={handleSignupClick}>
+              Sign in
+            </button>
+          </div>
+
+        </div>
       </div>
-     
-    </div>
-  </div>
-</div>
+    </div></>
   )}
 
 export default SystemLogin;
