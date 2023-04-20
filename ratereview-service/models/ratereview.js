44961c99a665ade195f903d1a91ee2a863054687
LochaniRanasinghe <lochaniranasinghe2000@gmail.com>
2023-04-21 00:13:11 +0530

    Rate review modify

---
+++ ratereview-service/models/ratereview.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const ratereviewSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    customerName: {
+        type: String,
+        required: true,
+    },
+    productId: {
+        type: String,
+        required: true,
+    },
+    sellerId: { 
+        type: String,
+        required: true,
+    },
+    sellerRating: {
+        type: Number,
+        required: true,
+    },
+    productRating: {
+        type: Number,
+        required: true,
+    },
+    productReview: {
+        type: String,
+        required: true,
+    },
+    sellerReview: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export const RateReview = mongoose.model("RateReview", ratereviewSchema);
