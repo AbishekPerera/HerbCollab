b5889acb6516cafc000b51cb6753f2c5d2990b6e
Janani Mayadunna <87350830+IT21026898@users.noreply.github.com>
2023-04-24 15:44:00 +0530

    delete products from frontend

--- frontend/src/pages/System/Seller/SellerProducts.jsx
+++ frontend/src/pages/System/Seller/SellerProducts.jsx
@@ -7,6 +7,7 @@ import AddProductModal from '../../../components/System/SellerProducts/AddProduc
 import UpdateProductModal from '../../../components/System/SellerProducts/UpdateProductModal';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import swal from 'sweetalert';
 
 const SellerProducts = () => {
   const [showAddProductModal, setShowAddProductModal] = useState(false);
@@ -19,6 +20,33 @@ const SellerProducts = () => {
   const handleUpdateProductModalShow = () => setShowUpdateProductModal(true);
 
   const [product, setProduct] = useState([]);
+
+  function deleteProduct(id) {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this record!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        //if true do the following
+        axios
+          .delete("http://localhost:8071/products/delete/" + id)
+          .then((res) => {
+            swal("Your product has been deleted!", {
+              icon: "success",
+            });
+            window.location.reload(false);
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
+      } else {
+        swal("Delete cancelled success!");
+      }
+    });
+  }
 
   const getProducts = () => {
     axios
@@ -74,15 +102,17 @@ const SellerProducts = () => {
                       />
                     {/* </div> */}
                     <ul class="d-flex align-items-center justify-content-center list-unstyled icons">
+                    <Link to={"/system/seller-products/update/" + product._id}>
                       <li
                         class="icon "
                         id="edit"
                       >
-                        <Link to={"/system/seller-products/update/" + product._id}>
+                       
                         <span class="bi bi-pen"></span>
-                        </Link>
                       </li>
-                      <li class="icon mx-3">
+                      </Link>
+
+                      <li class="icon mx-3" onClick={() => deleteProduct(product._id)}>
                         <span class="bi bi-trash"></span>
                       </li>
                     </ul>
