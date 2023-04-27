c9d774de08df0fe239c66479413ebc2c99b9fcef
Janani Mayadunna <87350830+IT21026898@users.noreply.github.com>
2023-04-27 16:39:53 +0530

    Admin -Dashboard changes

--- frontend/src/pages/System/Admin/Dashboard.jsx
+++ frontend/src/pages/System/Admin/Dashboard.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../../components/System/Sidebar/Sidebar";
 import "./styles/Dashboard.css";
 import SystemNav from "../../../components/System/SystemNavBar/SystemNav";
 import SystemFooter from "../../../components/System/SystemFooter/SystemFooter";
 import DashInfoCard from "../../../components/System/DashInfoCard/DashInfoCard";
+// import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+// import axios from 'axios';
 
 const Dashboard = () => {
+
   return (
     <div className="mainContainer">
       <div className="sidebar">
@@ -19,24 +22,20 @@ const Dashboard = () => {
         <div className="content">
           <DashInfoCard />
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et non odio
-          incidunt a veritatis voluptatem excepturi, magni, natus harum
-          perspiciatis sapiente alias? Ad voluptate tenetur nostrum eveniet
-          dolorem. Modi, ipsum. Placeat at voluptate, quisquam vero quibusdam ut
-          consequatur, magni beatae autem dolorem illum! Et doloremque eum
-          mollitia odit sunt, reiciendis eaque blanditiis. Nulla ad doloribus
-          inventore unde voluptatum nisi iusto! Voluptatibus facilis mollitia
-          recusandae repellendus nesciunt neque perferendis, exercitationem at
-          modi placeat magnam amet tempora cupiditate maiores accusantium iusto,
-          nostrum minima saepe voluptatum aut perspiciatis quasi rerum. Rem,
-          beatae quod. Odio laborum nihil vero, asperiores, sint facere itaque
-          velit iure dignissimos enim repellat quos dolorum harum cupiditate
-          facilis, veniam et incidunt delectus sequi. Laboriosam atque
-          distinctio nihil, nostrum voluptates ipsa? Magni omnis voluptate
-          quidem earum expedita dolore velit distinctio, dolor reprehenderit
-          voluptas autem labore dolorem ipsa atque ut? Velit mollitia hic eaque
-          aspernatur voluptas veniam recusandae eos similique maxime eligendi!
-        </div>
+          <div className="chartContainer">
+            
+          </div>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+          </div>
         <SystemFooter />
       </div>
     </div>
